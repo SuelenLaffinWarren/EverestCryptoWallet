c10ed79204cd0b74b1c0ea9c8dc5604fce1789d3
SuelenLaffinWarren <suelen.laffin@warren.com.br>
2022-09-12 19:04:58 -0300

    feat: add new files

---
+++ lib/present/wallet/widgets/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'package:everest_card2_listagem/present/wallet/widgets/wallet_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3)).then(
+      (value) => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) {
+            return const WalletScreen();
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Lottie.asset('assets/lottie/bitcoin2.json'),
+      ),
+    );
+  }
+}
